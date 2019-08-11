7c5719e0938a6b63bdad370167206fdc277813e5
Liam Howe <liam.howe@oddschecker.com>
2019-08-11 18:51:51 +0100

    Added response types and improved response

--- itemsapi/item/item.go
+++ itemsapi/item/item.go
@@ -1,16 +1,60 @@
 package item
 
-type Item struct {
+import (
+    "strconv"
+)
+
+type ItemResponse struct {
     ID int                  `json:"id,omitempty"`
     Name string             `json:"name,omitempty"`
-    Price Price             `json:"price,omitempty"`
-    NumberAvailable int     `json:"numberAvailable,omitempty"`
+    Price string            `json:"price,omitempty"`
+    NumberAvailable int     `json:"numberAvailable"`
+}
+
+type Item struct {
+    ID int
+    Name string
+    Price Price
+    NumberAvailable int
 }
 
 type Price struct {
-    Currency string         `json:"currency,omitempty"`
-    Integer int             `json:"integer,omitempty"`
-    Decimal int             `json:"decimal,omitempty"`
+    CurrencyCode string
+    Integer int
+    Decimal int
+}
+
+func (price Price) String() string {
+    currencySymbol := getCurrency(price.CurrencyCode).CurrencySymbol
+    priceString := currencySymbol + strconv.Itoa(price.Integer)
+    if price.Decimal > 0 {
+        priceString += "." + strconv.Itoa(price.Decimal)
+    }
+    return priceString
+}
+
+type Currency struct {
+    CurrencyCode string
+    CurrencySymbol string
+}
+
+var currencies = [...]Currency{
+    Currency{"GBP", "£"},
+    Currency{"EUR", "€"},
+    Currency{"USD", "$"},
+}
+
+func getCurrency(currencyCode string) *Currency {
+    for _, currency := range currencies {
+        if currency.CurrencyCode == currencyCode {
+            return &currency
+        }
+    }
+    return nil
+}
+
+func NewItemResponse(id int, name string, price string, numberAvailable int) ItemResponse {
+    return ItemResponse{id, name, price, numberAvailable}
 }
 
 func NewItem(id int, name string, price Price, numberAvailable int) Item {
--- itemsapi/itemsapi.go
+++ itemsapi/itemsapi.go
@@ -2,7 +2,7 @@ package main
 
 import (
     "fmt"
-    "github.com/LiamHowe/ebuy/itemsapi/itemsdao"
+    "github.com/LiamHowe/ebuy/itemsapi/itemsservice"
     "github.com/LiamHowe/ebuy/itemsapi/item"
     "encoding/json"
     "log"
@@ -11,14 +11,15 @@ import (
 )
 
 func GetItemsEndpoint(w http.ResponseWriter, req *http.Request) {
-    json.NewEncoder(w).Encode(itemsdao.GetItems())
+    json.NewEncoder(w).Encode(itemsservice.GetItems())
 }
 
 func main() {
     router := mux.NewRouter()
-    itemsdao.AddItem(item.NewItem(1, "toaster", item.NewPrice("GBP", 15, 0), 5))
-    itemsdao.AddItem(item.NewItem(2, "microwave", item.NewPrice("GBP", 25, 99), 0))
+    itemsservice.AddItem(item.NewItem(1, "toaster", item.NewPrice("GBP", 15, 0), 5))
+    itemsservice.AddItem(item.NewItem(2, "microwave", item.NewPrice("GBP", 25, 99), 0))
+    itemsservice.AddItem(item.NewItem(3, "sofa", item.NewPrice("USD", 425, 0), 0))
     router.HandleFunc("/items", GetItemsEndpoint).Methods("GET")
-    fmt.Println(itemsdao.GetItems())
+    fmt.Println(itemsservice.GetItems())
     log.Fatal(http.ListenAndServe(":1234", router))
 }
